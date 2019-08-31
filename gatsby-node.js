b028542cdae76761f9f3261ac6e9f3dccb8b2896
Yuraima <yurm04@gmail.com>
2019-08-31 19:50:30 -0400

    added default date to list overview

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,18 @@ const LISTS_ENDPT = "/lists"
 const LISTS_NAMES_ENDPT = "/names.json"
 const LISTS_OVERVIEW_ENDPT = '/overview.json'
 
+function getToday() {
+  var d = new Date(),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
 function getResults(resData) {
   return resData && resData.data && resData.data.results
 }
@@ -21,9 +33,7 @@ function getBookLists(options) {
 }
 
 function getListOverview(options) {
-  const { token, date } = options
-
-
+  const { token, date = getToday() } = options
 
   return axios(`${NYTIMES_API}${LISTS_ENDPT}${LISTS_OVERVIEW_ENDPT}?api-key=${token}&published_date=${date}`)
     .then(data => getResults(data))
